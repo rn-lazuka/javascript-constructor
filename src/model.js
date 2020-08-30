b3bd061a611bc81df1f550c2dbf6d8f8e7323763
rn-lazuka <rn.lazuka@gmail.com>
2020-08-30 21:22:06 +0300

    first commit

---
+++ src/model.js
@@ -0,0 +1,44 @@
+import image from './assets/image.png'
+import {TextColumnsBlock, TextBlock, TitleBlock, ImageBlock} from './classes/block'
+import {css} from './utils'
+
+const text = `
+Крутые видео и уроки по JavaScript тут: <a href="https://youtube.com/c/VladilenMinin" target="_blank">Владилен Минин</a>. Тут ты найдешь исчерпывающую информацию по любым аспектам языка, любым фреймворкам, такие как: React, Vue, Angular, Node, Svelte, Express, Next, Nuxt и многое другое. Присоединяйся!
+`
+
+export const model = [
+  new TitleBlock('Конструктор сайтов на чистом JavaScript!', {
+    tag: 'h2',
+    styles: css({
+      background: 'linear-gradient(to right, #ff0099, #493240)',
+      color: '#fff',
+      padding: '1.5rem',
+      'text-align': 'center'
+    })
+  }),
+  new ImageBlock(image, {
+    styles: css({
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center'
+    }),
+    alt: 'my image',
+    imageStyles: css({
+      width: '500px',
+      height: 'auto'
+    })
+  }),
+  new TextBlock(text, {styles: 'background: linear-gradient(to left, #f2994a, #f2c94c);font-weight: bold;padding: 1rem;'}),
+  new TextColumnsBlock([
+    'Приложение на чистом JavaScript, без использования библиотек',
+    'Принципы SOLID и ООП в JavaScript за один курс',
+    'JavaScript - это просто, интересно. '
+  ], {
+    styles: css({
+      padding: '2rem 0',
+      color: '#fff',
+      background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+      'font-weight': 'bold'
+    })
+  })
+]
